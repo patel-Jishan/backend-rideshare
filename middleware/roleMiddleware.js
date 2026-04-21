a1aff99a91f4fcd3a9811d73310ee902f0fe1aa5
Jishan Patel <jishanpatel.15072003@gmail.com>
2026-04-21 12:47:30 +0530

    Update roleMiddleware.js

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,12 +1,12 @@
 module.exports = (...roles) => {
   return (req, res, next) => {
     try {
-      // 🔐 Not logged in
+   
       if (!req.user) {
         return res.status(401).json({ message: "Unauthorized" });
       }
 
-      // 🚫 Role not allowed
+     
       if (!roles.includes(req.user.role)) {
         return res.status(403).json({
           message: `Role (${req.user.role}) not allowed`,
