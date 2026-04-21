30fac15dc64e2b914a001664f8ce884dc2fc6ed2
Jishan Patel <jishanpatel.15072003@gmail.com>
2026-04-21 12:47:59 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,19 +2,19 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    // Authorization header
+
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
       return res.status(401).json({ message: "No token provided" });
     }
 
-    // Format: Bearer TOKEN
+   
     const token = authHeader.startsWith("Bearer ")
       ? authHeader.split(" ")[1]
       : authHeader;
 
-    // Verify token
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Attach user data (id + role)
@@ -22,12 +22,12 @@ module.exports = (req, res, next) => {
 
     next();
   } catch (err) {
-    // Token expired
+
     if (err.name === "TokenExpiredError") {
       return res.status(401).json({ message: "Token expired" });
     }
 
-    // Invalid token
+ 
     return res.status(401).json({ message: "Invalid token" });
   }
 };
